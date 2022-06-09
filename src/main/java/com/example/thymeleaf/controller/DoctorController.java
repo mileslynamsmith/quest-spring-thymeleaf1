b74e42792a3b26e2d04016565da31afc0972eef6
Miles Lynam-Smith <miles.lynam-smith@telekom.de>
2022-06-09 11:05:36 +0200

    first commit thymeleaf

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,14 +1,29 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DoctorController {
 
     @GetMapping("/doctor/")
-    public String doctor() {
-
+    public String doctor(Model model,
+                        @RequestParam(value="name",
+                                required=false,
+                                defaultValue="John Smith") String name
+    ){
+    model.addAttribute("name", name); 
         return "doctor";
     }
-}
+    @GetMapping("/doctor/")
+    public Integer doctor(Model model,
+                        @RequestParam(value="number",
+                                required=false,
+                                defaultValue="0") Integer number
+    ){
+    model.addAttribute("number", number);
+        return "doctor";
+    }
+
