26f1a088639f97834d75fc772285d561859c5620
Miles Lynam-Smith <miles.lynam-smith@telekom.de>
2022-06-09 14:04:58 +0200

    final changes

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -10,20 +10,11 @@ public class DoctorController {
 
     @GetMapping("/doctor/")
     public String doctor(Model model,
-                        @RequestParam(value="name",
-                                required=false,
-                                defaultValue="John Smith") String name
-    ){
-    model.addAttribute("name", name); 
-        return "doctor";
-    }
-    @GetMapping("/doctor/")
-    public Integer doctor(Model model,
-                        @RequestParam(value="number",
-                                required=false,
-                                defaultValue="0") Integer number
-    ){
-    model.addAttribute("number", number);
+            @RequestParam(value = "name", required = false, defaultValue = "John Smith") String name,
+            @RequestParam(value = "number", required = false, defaultValue = "0") Integer number) {
+        model.addAttribute("name", name);
+        model.addAttribute("number", number);
         return "doctor";
     }
 
+}
